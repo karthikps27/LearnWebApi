655bb48bc6e210ff93727d3860c163cf71fd5ada
Karthik Soorya <karthikps27@gmail.com>
2020-06-24 10:27:27 +1000

    Serilog configuration

--- src/BookMaster/Program.cs
+++ src/BookMaster/Program.cs
@@ -1,7 +1,11 @@
 using BookMaster.Data.Framework;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace LearnWebApi
 {
@@ -9,11 +13,20 @@ namespace LearnWebApi
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
